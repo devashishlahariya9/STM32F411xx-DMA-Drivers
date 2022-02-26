64f856d140df67bfc7ea578e6d4e8b8cf205a4d8
Devashish Lahariya <devashishlahariya9@gmail.com>
2022-02-27 01:08:52 +0530

    Update main.c

--- src/Src/main.c
+++ src/Src/main.c
@@ -9,7 +9,7 @@ int main(void)
 	DMA2_CLK_ENABLE();
 
 	DMA_StreamInit(DMA2_STREAM3, DMA_Channel3, DATA_8_BIT, DATA_8_BIT, MEM_TO_MEM, FIXED_MODE, DMA_LOW_PRIORITY);
-	DMA_SetDataAddress(DMA2_STREAM3, (uint32_t)&c2, (uint32_t)&c1);
+	DMA_SetDataAddress(DMA2_STREAM3, (uint32_t*)&c2, (uint32_t*)&c1);
 	DMA_SetNumberOfDataItems(DMA2_STREAM3, 1);
 
 	DMA_EnableStream(DMA2_STREAM3);
